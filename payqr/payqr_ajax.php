cecb0a8818f32aec5441863b5b9cf817b7119350
rakot9 <sobolev@payqr.ru>
2015-12-15 12:45:00 +0300

    clean cart

--- payqr/payqr_ajax.php
+++ payqr/payqr_ajax.php
@@ -39,7 +39,7 @@ switch($_GET['action']) {
         //PayqrLog::log("payqr_ajax.php Очистка корзины!");
         echo "payqr_ajax.php Очистка корзины!";
 
-        DB::query("DELETE FROM {shop_cart} WHERE user_id=%d AND trash='0'", $this->diafan->_users->id);
+        DB::query("DELETE FROM {shop_cart} WHERE user_id=%d AND trash='0'", $diafan->_users->id);
         //Очищаем сессию
         if(isset($_SESSION["cart"]))
         {
